2c6a60f10edf3e1169da6ab25322e1bb7bf5ee75
zhuo <1465701009@qq.com>
2025-04-17 22:08:27 +0800

    fix：依赖注入

--- piper-ai-engine/piper-ai-engine-common/src/main/java/com/zhuo/piper/scheduler/chain/task/LocalMessageSecondScheduler.java
+++ piper-ai-engine/piper-ai-engine-common/src/main/java/com/zhuo/piper/scheduler/chain/task/LocalMessageSecondScheduler.java
@@ -5,7 +5,9 @@ import com.zhuo.piper.save.LocalMessageService;
 import com.zhuo.piper.scheduler.chain.AbstractSchedulerChain;
 import com.zhuo.piper.struct.DAG;
 import jakarta.annotation.Resource;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LocalMessageSecondScheduler extends AbstractSchedulerChain {
     @Resource
     private LocalMessageService localMessageService;
